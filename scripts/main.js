0234028df459169ca1eb5f8bdc45c85b3430a7ce
airatfunduk <77188340+airatfunduk@users.noreply.github.com>
2021-03-02 15:16:28 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+var myImage = document.querySelector('img')//объявили переменную; 
+myImage.onclick = function(){
+var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/macos.jpg') {
+      myImage.setAttribute ('src','images/macos3.jpg');
+    } else {
+      myImage.setAttribute ('src','images/macos.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to the party, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to the party, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
